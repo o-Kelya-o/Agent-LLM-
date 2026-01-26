fee2f397bf3516997f6d4fe6b5e053aa3c696142
kelya <kellyanhumbert@gmail.com>
2026-01-26 23:34:17 +0100

    Edited main.my : added code to work on the prompts and ai. Added a calculator folder

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -11,7 +13,25 @@ def main():
     
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(model = "gemini-2.5-flash", contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    parser = argparse.ArgumentParser(description="Chat Bot")
+    parser.add_argument("user_prompt", type=str, help="The message the ai will read")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+
+    response = client.models.generate_content(model = "gemini-2.5-flash", contents = messages)
+    metadata = response.usage_metadata
+
+    if metadata == None:
+        raise RuntimeError("The metadata if None, there's likely an issue with Google api... retry in a moment")
+    
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {metadata.prompt_token_count}")
+        print(f"Response tokens: {metadata.candidates_token_count}")
+        
     print(response.text)
 
 if __name__ == "__main__":
